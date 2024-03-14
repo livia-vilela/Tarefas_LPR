d0bdf0f60ae4928a32c69022c2cb4576f4bd95b0
livia-vilela <liviavilelaribeiro@gmail.com>
2024-03-14 15:02:25 -0300

    Leitor_de_Números.cs
    Exercício 01 da Aula 05

---
+++ Leitor_Numeros.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Leitor_Numeros
+{
+    static void Main(string[] args)
+    {
+        Console.Write(">> Digite um número inteiro: ");
+        int numero = int.Parse(Console.ReadLine());
+        if (numero % 2 != 0)
+        {
+            Console.WriteLine("\n > O número {0} é ÍMPAR.", numero);
+        }
+        else
+        {
+            Console.WriteLine("\n > O número {0} é PAR.", numero);
+        }
+    }
+}
